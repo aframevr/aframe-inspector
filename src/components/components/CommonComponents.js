626b3789cb68f73ca231a1b79b1eadc7b634e0d7
Fernando Serrano <fernandojsg@gmail.com>
2016-07-11 23:07:11 +0200

    Expose DEFAULT_COMPONENTS in CommonComponents

--- src/components/components/CommonComponents.js
+++ src/components/components/CommonComponents.js
@@ -4,7 +4,7 @@ import PropertyRow from './PropertyRow';
 import Collapsible from '../Collapsible';
 import Mixins from './Mixins';
 import {updateEntity} from '../../actions/entity';
-const Events = require('../../lib/Events.js');
+import Clipboard from 'clipboard';
 
 // @todo Take this out and use updateEntity?
 function changeId (entity, componentName, propertyName, value) {
@@ -14,45 +14,64 @@ function changeId (entity, componentName, propertyName, value) {
   }
 }
 
-/**
- * Core component properties such as id, position, rotation, scale.
- */
-const CommonComponents = props => {
-  const entity = props.entity;
-  const components = entity ? props.entity.components : {};
-  if (!entity) { return <div></div>; }
-  return (
-    <Collapsible>
-      <div className='collapsible-header'>
-        <span>Common</span>
-      </div>
-      <div className='collapsible-content'>
-        <div className='row'>
-          <span className='value tagName'><code>&lt;{entity.tagName.toLowerCase()}&gt;</code></span>
+class CommonComponents extends React.Component {
+  static propTypes = {
+    entity: React.PropTypes.object
+  };
+
+  componentDidMount () {
+    var clipboard = new Clipboard('[data-action="copy-entity-to-clipboard"]', {
+      text: trigger => {
+        return this.props.entity.outerHTML;
+      }
+    });
+    clipboard.on('error', e => {
+      // @todo Show the error on the UI
+    });
+  }
+
+  renderCommonAttributes () {
+    const entity = this.props.entity;
+    const components = entity ? entity.components : {};
+    return Object.keys(components).filter(function (key) {
+      return ['visible', 'position', 'scale', 'rotation'].indexOf(key) !== -1;
+    }).map(componentName => {
+      const componentData = components[componentName];
+      const schema = AFRAME.components[componentName].schema;
+      return (
+        <PropertyRow onChange={updateEntity} key={componentName} name={componentName}
+          showHelp={true} schema={schema} data={componentData.data}
+          componentname={componentName} entity={entity}/>
+      );
+    })
+  }
+
+  render () {
+    const entity = this.props.entity;
+    if (!entity) { return <div></div>; }
+    return (
+      <Collapsible>
+        <div className='collapsible-header'>
+          <span>Common</span>
+          <div>
+            <a href='#' title='Copy entity HTML to clipboard' data-action='copy-entity-to-clipboard'
+              className='button fa fa-clipboard' onClick={event => event.stopPropagation()}></a>
+          </div>
         </div>
-        <div className='row'>
-          <span className='text'>ID</span>
-          <InputWidget onChange={changeId} entity={entity} name='id' value={entity.id}/>
+        <div className='collapsible-content'>
+          <div className='row'>
+            <span className='value tagName'><code>&lt;{entity.tagName.toLowerCase()}&gt;</code></span>
+          </div>
+          <div className='row'>
+            <span className='text'>ID</span>
+            <InputWidget onChange={changeId} entity={entity} name='id' value={entity.id}/>
+          </div>
+          {this.renderCommonAttributes()}
+          <Mixins entity={entity}/>
         </div>
-        {
-          Object.keys(components).filter(function (key) {
-            return ['visible', 'position', 'scale', 'rotation'].indexOf(key) !== -1;
-          }).map(function (key) {
-            const componentData = components[key];
-            const schema = AFRAME.components[key].schema;
-            return (
-              <PropertyRow onChange={updateEntity} key={key} name={key}
-                showHelp={true} schema={schema} data={componentData.data}
-                componentname={key} entity={props.entity}/>
-            );
-          })
-        }
-        <Mixins entity={entity}/>
-      </div>
-    </Collapsible>
-  );
-};
-CommonComponents.propTypes = {
-  entity: React.PropTypes.object
-};
+      </Collapsible>
+    );
+  }
+}
+
 export default CommonComponents;
