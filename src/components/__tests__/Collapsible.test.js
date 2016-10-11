a1fd8b0442842abc61acaf003bde580d03cbf0ce
Kevin Ngo <me@ngokevin.com>
2016-10-11 11:08:36 -0700

    start unit tests

---
+++ src/components/__tests__/Collapsible.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Collapsible from '../Collapsible.js';
+
+describe('Collapsible', () => {
+ it('does not set class when not collapsed', () => {
+    const tree = renderer.create(
+      <Collapsible collapsed={false}>
+        <div/><div/>
+      </Collapsible>
+    ).toJSON();
+    expect(tree.children[1].props.className).not.toContain('hide');
+  });
+
+  it('sets class when collapsed', () => {
+    const tree = renderer.create(
+      <Collapsible collapsed={true}>
+        <div/><div/>
+      </Collapsible>
+    ).toJSON();
+    expect(tree.children[1].props.className).toContain('hide');
+  });
+});
