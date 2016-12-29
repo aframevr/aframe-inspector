b1a293955930405cfbf839c04c5ca9ac00335c9d
Fernando Serrano <fernandojsg@gmail.com>
2016-12-30 00:14:53 +0100

    Fixes #404 Color input

--- src/components/widgets/ColorWidget.js
+++ src/components/widgets/ColorWidget.js
@@ -15,34 +15,69 @@ export default class ColorWidget extends React.Component {
 
   constructor (props) {
     super(props);
-    this.state = {value: correctColorValue(this.props.value) || ''};
+
+    var value = this.props.value;
+    this.color = new THREE.Color();
+
+    this.state = {
+      value: value,
+      pickerValue: this.getHexString(value)
+    };
   }
 
-  componentWillReceiveProps (newProps) {
-    if (newProps.value !== this.state.value) {
-      this.setState({value: correctColorValue(newProps.value)});
-    }
-  }
+  setValue (value) {
+    var pickerValue = this.getHexString(value);
 
-  onChange = e => {
-    var value = e.target.value;
-    this.setState({value: value});
+    this.state = {
+      value: value,
+      pickerValue: pickerValue
+    };
+
     if (this.props.onChange) {
       this.props.onChange(this.props.name, value);
     }
   }
 
+  componentWillReceiveProps (newProps) {
+    if (newProps.value !== this.state.value) {
+      this.setState({
+        value: newProps.value,
+        pickerValue: this.getHexString(newProps.value)
+      });
+    }
+  }
+
+  getHexString (value) {
+     return '#' + this.color.set(value).getHexString();
+  }
+
+  onChange = e => {
+    this.setValue(e.target.value);
+  }
+
+  onKeyUp = e => {
+    e.stopPropagation();
+    // if (e.keyCode === 13)
+    {
+      this.setValue(e.target.value);
+    }
+  }
+
+  onChangeText = e => {
+    this.setState({value: e.target.value});
+  }
+
   render () {
     return (
-      <input type='color' className="color" value={this.state.value}
-        title={this.state.value} onChange={this.onChange}/>
+      <span className='color-widget'>
+        <input type='color' className="color" value={this.state.pickerValue}
+          title={this.state.value} onChange={this.onChange}/>
+        <input type="text" className="color_value"
+          value={this.state.value}
+          onKeyUp={this.onKeyUp}
+          onChange={this.onChangeText}
+        />
+      </span>
     );
   }
-}
-
-function correctColorValue (value) {
-  if (value.length === 4) {
-    value = '#' + value[1] + value[1] + value[2] + value[2] + value[3] + value[3];
-  }
-  return value;
 }
