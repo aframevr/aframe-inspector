8af28ddb79dfcaa8f6c6a4a21c61e6e56e8b0ee2
Fernando Serrano <fernandojsg@gmail.com>
2017-02-09 14:29:52 +0100

    Fixes #145 add icon to toggle visibility and modify scenegraph style

--- src/lib/shortcuts.js
+++ src/lib/shortcuts.js
@@ -81,6 +81,7 @@ module.exports = {
     }
   },
   onKeyDown: function (event) {
+
     if (event.ctrlKey && os === 'windows' || event.metaKey && os === 'macos') {
       if (AFRAME.INSPECTOR.selectedEntity && document.activeElement.tagName !== 'INPUT') {
         // x: cut selected entity
