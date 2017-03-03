d212de8ce89ac325cbe7c25ca6dcdf32a07eec1e
Fernando Serrano <fernandojsg@gmail.com>
2017-03-03 21:54:21 +0100

    Fixes #458 prevent tab,1,2 to be captured inside an input box

--- src/lib/shortcuts.js
+++ src/lib/shortcuts.js
@@ -60,6 +60,7 @@ module.exports = {
     }
   },
   onKeyDown: function (event) {
+    if (!shouldCaptureKeyEvent(event)) { return; }
 
     if (event.ctrlKey && os === 'windows' || event.metaKey && os === 'macos') {
       if (AFRAME.INSPECTOR.selectedEntity && document.activeElement.tagName !== 'INPUT') {
