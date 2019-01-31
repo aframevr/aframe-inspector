b371202e68dcad997e3f740996f912f79fe77526
Kevin Ngo <me@ngokevin.com>
2019-01-31 12:57:00 -0800

    tie ortho camera pan speed to zoom

--- src/lib/EditorControls.js
+++ src/lib/EditorControls.js
@@ -135,7 +135,12 @@ THREE.EditorControls = function(_object, domElement) {
   };
 
   this.pan = function(delta) {
-    var distance = object.position.distanceTo(center);
+     var distance;
+    if (this.isOrthographic) {
+      distance = Math.abs(object.right);
+    } else {
+      distance = object.position.distanceTo(center);
+    }
 
     delta.multiplyScalar(distance * scope.panSpeed);
     delta.applyMatrix3(normalMatrix.getNormalMatrix(object.matrix));
