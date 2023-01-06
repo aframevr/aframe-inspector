12325018fa9db9ed0c02ab1d317c1eaf8d53d689
Vincent Fretin <vincent.fretin@gmail.com>
2023-01-06 11:52:03 +0100

    Remove double spaces when copying component representation (#671)

--- src/lib/entity.js
+++ src/lib/entity.js
@@ -514,9 +514,7 @@ export function getComponentClipboardRepresentation(entity, componentName) {
   }
 
   const diff = getModifiedProperties(entity, componentName);
-  const attributes = AFRAME.utils.styleParser
-    .stringify(diff)
-    .replace(/;|:/g, '$& ');
+  const attributes = AFRAME.utils.styleParser.stringify(diff);
   return `${componentName}="${attributes}"`;
 }
 
