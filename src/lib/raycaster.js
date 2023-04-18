dc3c485c95ddcdde7bf50a4fd7f3e221b1772cee
Vincent Fretin <vincent.fretin@gmail.com>
2023-04-18 03:14:10 +0200

    Properly unregister listeners when closing inspector and register them back when opening again (#683)

--- src/lib/raycaster.js
+++ src/lib/raycaster.js
@@ -127,9 +127,15 @@ export function initRaycaster(inspector) {
     el: mouseCursor,
     enable: () => {
       mouseCursor.setAttribute('raycaster', 'enabled', true);
+      inspector.container.addEventListener('mousedown', onMouseDown);
+      inspector.container.addEventListener('mouseup', onMouseUp);
+      inspector.container.addEventListener('dblclick', onDoubleClick);
     },
     disable: () => {
       mouseCursor.setAttribute('raycaster', 'enabled', false);
+      inspector.container.removeEventListener('mousedown', onMouseDown);
+      inspector.container.removeEventListener('mouseup', onMouseUp);
+      inspector.container.removeEventListener('dblclick', onDoubleClick);
     }
   };
 }
