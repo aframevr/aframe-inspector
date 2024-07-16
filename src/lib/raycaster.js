2b800d77d26388515a6bc2de3d08d8a29c82e5fb
Vincent Fretin <vincent.fretin@gmail.com>
2024-07-16 12:40:32 +0200

    Remove unused onDoubleClickPosition

--- src/lib/raycaster.js
+++ src/lib/raycaster.js
@@ -55,7 +55,6 @@ export function initRaycaster(inspector) {
 
   const onDownPosition = new THREE.Vector2();
   const onUpPosition = new THREE.Vector2();
-  const onDoubleClickPosition = new THREE.Vector2();
 
   function onMouseEnter() {
     Events.emit(
@@ -110,12 +109,6 @@ export function initRaycaster(inspector) {
    * Focus on double click.
    */
   function onDoubleClick(event) {
-    const array = getMousePosition(
-      inspector.container,
-      event.clientX,
-      event.clientY
-    );
-    onDoubleClickPosition.fromArray(array);
     const intersectedEl = mouseCursor.components.cursor.intersectedEl;
     if (!intersectedEl) {
       return;
