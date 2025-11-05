c12f87803f36f1b3b13c01e02daf39c7989b48fb
Vincent Fretin <vincent.fretin@gmail.com>
2025-11-05 10:57:07 +0100

    Fix editing entity id, trigger the update only on blur and enter, not on each keystroke (fix #51) (#52)

--- src/components/widgets/InputWidget.js
+++ src/components/widgets/InputWidget.js
@@ -5,6 +5,7 @@ export default class InputWidget extends React.Component {
   static propTypes = {
     id: PropTypes.string,
     name: PropTypes.string.isRequired,
+    onBlur: PropTypes.func,
     onChange: PropTypes.func,
     value: PropTypes.any
   };
@@ -12,13 +13,31 @@ export default class InputWidget extends React.Component {
   constructor(props) {
     super(props);
     this.state = { value: this.props.value || '' };
+    this.input = React.createRef();
   }
 
-  onChange = (e) => {
-    var value = e.target.value;
+  onChange = (event) => {
+    const value = event.target.value;
     this.setState({ value: value });
     if (this.props.onChange) {
       this.props.onChange(this.props.name, value);
+    }
+  };
+
+  onBlur = (event) => {
+    if (this.props.onBlur) {
+      const value = event.target.value;
+      this.props.onBlur(this.props.name, value);
+    }
+  };
+
+  onKeyDown = (event) => {
+    event.stopPropagation();
+
+    // enter
+    if (event.keyCode === 13) {
+      this.input.current.blur();
+      return;
     }
   };
 
@@ -32,10 +51,13 @@ export default class InputWidget extends React.Component {
     return (
       <input
         id={this.props.id}
+        ref={this.input}
         type="text"
         className="string"
         value={this.state.value}
+        onBlur={this.onBlur}
         onChange={this.onChange}
+        onKeyDown={this.onKeyDown}
       />
     );
   }
