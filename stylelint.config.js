b519e0e4cd471465556dce0981864a7013859116
Vincent Fretin <vincent.fretin@gmail.com>
2025-12-20 12:12:26 +0100

    Fix the npm run lint:css command

---
+++ stylelint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['stylelint-config-standard'],
+  plugins: ['stylelint-order'],
+  ignoreFiles: ['src/style/themes.css'],
+  rules: {
+    'declaration-property-value-no-unknown': null,
+    'import-notation': null,
+    'media-feature-range-notation': 'prefix',
+    'no-descending-specificity': null,
+    'no-invalid-position-at-import-rule': null,
+    'selector-class-pattern': null,
+    'selector-id-pattern': null,
+    'value-keyword-case': null,
+    'order/properties-alphabetical-order': true
+  },
+  overrides: [
+    {
+      files: ['**/*.styl'],
+      customSyntax: 'postcss-styl'
+    }
+  ]
+};
